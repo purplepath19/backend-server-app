d2f48160f34f5d6a46246bc762c427b2aa3cc60d
Shannon <shannonblas@pucpr.edu>
2024-04-01 10:52:56 -0400

    feat: added post crud

--- routes/schools.js
+++ routes/schools.js
@@ -1,34 +1,103 @@
-const SchoolModel = require ("../models/SchoolModel");
+const SchoolModel = require("../models/SchoolModel");
 var express = require("express");
-const mongoose = require ('mongoose');
+const mongoose = require("mongoose");
 
 //Instance of Express Router
 const schoolRouter = express.Router();
 
-
 //Get the schools
 
-schoolRouter.post("/",  async function (req, res, next){
-console.log(req.body);
-const { position, title, content } = req.body //extract data from req body 
+// POST 
+schoolRouter.post("/", async function (req, res, next) {
+  console.log(req.body);
+  const { position, name, content } = req.body; //extract data from req body
 
-//New instance of the school model? 
-const newSchool = new SchoolModel({
-    position: position,
-    title: title,
-    content: content,
+
+
+SchoolModel.findOne({ name })
+    .then((foundSchool) => {
+      // If the user with the same email already exists, send an error response
+      if (foundSchool) {
+        console.log("foundSchool", foundSchool);
+        res.status(400).json({ message: "School name already exists." });
+        return;
+      }
+
+      // Create a new user in the database
+      // We return a pending promise, which allows us to chain another `then`
+      SchoolModel.create({ position, name, content })
+        .then((createdSchool) => {
+          // Deconstruct the newly created user object to omit the password
+          // We should never expose passwords publicly
+          const { position, name, content, _id } = createdSchool;
+
+          // Create a new object that doesn't expose the password
+          const school = { position, name, content, _id };
+
+          // Send a json response containing the user object
+          res.status(201).json({ school: school });
+        })
+        .catch((err) => {
+          if (err instanceof mongoose.Error.ValidationError) {
+            console.log("Error:", err);
+            res.status(501).json({ message: "Provide all fields", err });
+          } else if (err.code === 1100) {
+            console.log("Duplicate value", err);
+            res.status(502).json({ message: "invalid name" });
+          }
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
-//Save to database
-const savedSchool = await newSchool.save();
 
 
+// // UPDATE 
 
-})
+// userRouter.put("/:school", function(req, res, next) {
+//     const { school } = req.params;
+//     console.log("school", school, req.body);
+//     User.findOneAndUpdate({ school: school }, req.body, { new: true })
+//         .then(updatedSchool => {
+//             console.log("Update School here --->", updatedSchool);
+//             if (updatedSchool) {
+//                 console.log("UPDATED!!! ===>", updatedSchool);
+//                 res.status(201).json(updatedSchool);
+//             } else {
+//                 console.log("Not found");
+//                 res.status(400).json({ message: "not found" });
+//             }
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.status(500).json({ message: "Error: Updating Schools" });
+//         });
+// });
 
 
+//   //DELETE
+//   userRouter.delete("/:school", function(req, res, next) {
+//     const { school } = req.params;
+//     console.log("Deleting school:", school);
+//     User.findOneAndDelete({ school: school })
+//         .then(deletedSchool => {
+//             if (deletedSchool) {
+//                 console.log("Deleted school:", deletedSchool);
+//                 res.status(200).json({ message: "School deleted successfully" });
+//             } else {
+//                 console.log("School not found");
+//                 res.status(404).json({ message: "School not found" });
+//             }
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.status(500).json({ message: "Error: Deleting School" });
+//         });
+// });
 
-//Create schools in the database 
-SchoolModel.create({school})
+
 
 module.exports = schoolRouter;
